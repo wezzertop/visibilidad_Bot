c5df0a507fd9899c557ac3e7000c6632022a3fa6
Daniel <wezzertop@gmail.com>
2026-06-06 14:25:33 -0600

    Update module price to 5.00 USD

--- visibilidad_Bot/__manifest__.py
+++ visibilidad_Bot/__manifest__.py
@@ -18,7 +18,7 @@ Take control of your Livechat!
         'views/bot_menus.xml',
     ],
     'images': ['static/description/banner.gif', 'static/description/icon.png'],
-    'price': 20.00,
+    'price': 5.00,
     'currency': 'USD',
     'installable': True,
     'application': True,
